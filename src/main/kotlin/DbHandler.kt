79e1fed4e254cbfb09b91f47e77dae4697260710
UristLikot <33843680+UristLikot@users.noreply.github.com>
2019-07-09 16:33:32 +0300

    Initial commit

---
+++ src/main/kotlin/DbHandler.kt
@@ -0,0 +1,123 @@
+import org.spongepowered.api.Sponge
+import org.spongepowered.api.service.sql.SqlService
+import java.lang.Exception
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException
+import java.text.SimpleDateFormat
+import javax.sql.DataSource;
+
+class DbHandler {
+    var sql: SqlService? = null
+    @Throws(SQLException::class)
+    fun getDataSource(jdbcUrl: String): DataSource {
+        if (sql == null) {
+            sql = Sponge.getServiceManager().provide(SqlService::class.java).get()
+        }
+        return sql!!.getDataSource(jdbcUrl)
+    }
+
+    @Throws(SQLException::class)
+    fun createDb() {
+        val uri = Sponge.getServiceManager().provide(SqlService::class.java).get().getConnectionUrlFromAlias("main").get()
+        val cr = "CREATE DATABASE IF NOT EXISTS minecraft"
+        val sql =
+                "create table IF NOT EXISTS minecraft.bans\n" +
+                        "(\n" +
+                        "    uuid    text     null,\n" +
+                        "    name    text     null,\n" +
+                        "    created datetime null,\n" +
+                        "    source  text     null,\n" +
+                        "    expires datetime null,\n" +
+                        "    reason  text     null\n" +
+                        ");"
+        getDataSource(uri).connection.use { conn ->
+            conn.prepareStatement(cr).use { stmt ->
+                stmt.executeQuery().use { results ->
+                    println(results)
+                    conn.close()
+                }
+            }
+        }
+        getDataSource(uri).connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                stmt.executeQuery().use { results ->
+                    println(results)
+                    conn.close()
+                }
+            }
+        }
+
+    }
+
+    @Throws(SQLException::class)
+    fun addBanToDb(uuid: String, name: String, created: String, source: String, expires: String, reason: String) {
+        val uri = Sponge.getServiceManager().provide(SqlService::class.java).get().getConnectionUrlFromAlias("main").get()
+
+        val sql = "insert into minecraft.bans (bans.uuid, bans.name, bans.created, bans.source, bans.expires, bans.reason )values ('$uuid','$name','$created','$source','$expires','$reason')"
+        getDataSource(uri).connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                try {
+                    stmt.executeQuery().use { results ->
+                        println("already exist")
+                        conn.close()
+                    }
+                } catch (e: SQLIntegrityConstraintViolationException) {
+                    println(e.message)
+                }
+                conn.close()
+            }
+
+        }
+
+    }
+
+    @Throws(SQLException::class)
+    fun removeFromDb(name: String) {
+        val uri = Sponge.getServiceManager().provide(SqlService::class.java).get().getConnectionUrlFromAlias("main").get()
+        val sql = "DELETE FROM minecraft.bans where name='$name'"
+        getDataSource(uri).connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                try {
+                    stmt.executeQuery().use { results ->
+                        println("deleted")
+                        conn.close()
+                    }
+                } catch (e: SQLIntegrityConstraintViolationException) {
+                    println("do not exist")
+                    conn.close()
+                }
+                conn.close()
+            }
+
+        }
+    }
+
+    @Throws(SQLException::class)
+    fun refrBansDb(mp: MutableMap<String, String>) {
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val name = mp.get("name")
+        val uuid = mp.get("uuid")
+        val created = formatter.format(formatter.parse(mp.get("created")))
+        val source = mp.get("source")
+        val expires = formatter.format(formatter.parse(mp.get("expires")))
+        val reason = mp.get("reason")
+        val uri = Sponge.getServiceManager().provide(SqlService::class.java).get().getConnectionUrlFromAlias("main").get()
+        val sql = "insert into minecraft.bans (bans.uuid, bans.name, bans.created, bans.source, bans.expires, bans.reason )values ('$uuid','$name','$created','$source','$expires','$reason')"
+        getDataSource(uri).connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                try {
+                    stmt.executeQuery().use { results ->
+                        println("refresh complete")
+                        conn.close()
+                    }
+                } catch (e: SQLIntegrityConstraintViolationException) {
+                    println("already in db")
+                    conn.close()
+                }
+                conn.close()
+            }
+
+        }
+    }
+}
---
+++ src/main/kotlin/MCPlugin.kt
@@ -0,0 +1,216 @@
+import com.google.common.reflect.TypeToken
+import com.google.inject.Inject
+import ninja.leaping.configurate.ConfigurationNode
+import ninja.leaping.configurate.commented.CommentedConfigurationNode
+import ninja.leaping.configurate.gson.GsonConfigurationLoader
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader
+import ninja.leaping.configurate.loader.ConfigurationLoader
+import org.slf4j.Logger
+import org.spongepowered.api.Sponge
+import org.spongepowered.api.Sponge.getCommandManager
+import org.spongepowered.api.command.CommandResult
+import org.spongepowered.api.command.CommandSource
+import org.spongepowered.api.command.args.CommandContext
+import org.spongepowered.api.command.args.GenericArguments
+import org.spongepowered.api.command.spec.CommandExecutor
+import org.spongepowered.api.command.spec.CommandSpec
+import org.spongepowered.api.config.ConfigDir
+import org.spongepowered.api.config.DefaultConfig
+import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.game.state.GameStartedServerEvent
+import org.spongepowered.api.event.game.state.GameStartingServerEvent
+import org.spongepowered.api.event.network.ClientConnectionEvent
+import org.spongepowered.api.event.user.BanUserEvent
+import org.spongepowered.api.plugin.Plugin
+import org.spongepowered.api.profile.ProfileNotFoundException
+import org.spongepowered.api.service.ban.BanService
+import org.spongepowered.api.text.Text
+import org.spongepowered.api.util.ban.Ban
+import org.spongepowered.api.util.ban.BanTypes
+import java.io.File
+import java.nio.file.Path
+import java.sql.Array
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+import java.text.SimpleDateFormat
+import kotlin.collections.ArrayList
+import kotlin.collections.LinkedHashMap
+import kotlin.reflect.typeOf
+
+val db = DbHandler()
+
+@Plugin(id = "abplugin", name = "abplugin", version = "0.0.1", description = "My first plugin")
+class mcplugin {
+    @Inject
+    lateinit var logger: Logger
+
+    @Listener
+    fun onServerStart(event: GameStartedServerEvent) {
+        db.createDb()
+        logger.info("Successfully running abplugin.")
+
+
+    }
+
+
+    @Listener
+    fun loadToDb(event: BanUserEvent) {
+        event.ban.profile
+        val uuid = event.ban.profile.uniqueId.toString()
+        val name = event.ban.profile.name.get()
+        val created = event.ban.creationDate
+        val myDateCr = Date.from(created)
+        var source = event.ban.banSource.get().toString()
+        val expires = event.ban.expirationDate.get()
+        val myDateExp = Date.from(expires)
+        val reason = event.ban.reason.get().toString().replace("Text{", "").replace("}", "")
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val finCr = formatter.format(myDateCr)
+        val finExp = formatter.format(myDateExp)
+        if ("Unknown" in source) {
+            source = "Server"
+        }
+        db.addBanToDb(uuid, name, finCr, source, finExp, reason)
+    }
+
+    @Listener
+    fun checkBans(event: ClientConnectionEvent.Join) {
+        if (Sponge.getServer().getPlayer(event.targetEntity.name).isPresent) {
+            val playerProfile = Sponge.getServer().getPlayer(event.targetEntity.name).get().profile
+            val playerName = Sponge.getServer().getPlayer(event.targetEntity.name).get().name
+            if (service.isBanned(playerProfile)) {
+                val optionalBan = service.getBanFor(playerProfile)
+                if (optionalBan.isPresent) {
+                    val profileBan = optionalBan.get()
+                    val optionalReason = profileBan.reason
+                    if (optionalReason.isPresent) {
+                        val banReason = optionalReason.get()
+                        Sponge.getServer().getPlayer(playerName).get().kick(banReason)
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Listener
+    fun regBan(event: GameStartingServerEvent) {
+        logger.info("Successfully registered aban.")
+        try {
+            getCommandManager().register(this, banCommand, "aban")
+        } catch (e: IllegalArgumentException) {
+
+        }
+
+    }
+
+    @Listener
+    fun refrBan(event: GameStartingServerEvent) {
+        logger.info("Successfully registered refresh.")
+        try {
+            getCommandManager().register(this, refrBanCommand, "refrbans")
+        } catch (e: IllegalArgumentException) {
+
+        }
+
+    }
+
+    @Listener
+    fun reguBan(event: GameStartingServerEvent) {
+        logger.info("Successfully registered uban.")
+        try {
+            getCommandManager().register(this, ubanCommand, "uban")
+        } catch (e: IllegalArgumentException) {
+
+        }
+
+    }
+
+}
+
+class BanCommand : CommandExecutor {
+    override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+        try {
+            val pName = args.getOne<String>("player").get()
+            val pn = Sponge.getServer().gameProfileManager.get(pName).get()
+            val message = args.getAll<String>("reason").toString().replace("[", "").replace("]", "")
+            val time = args.getAll<Double>("time(hours)").toString().replace("[", "").replace("]", "").toDouble()
+            val instant = Instant.ofEpochMilli(Date().getTime()).plus(Duration.ofHours(time.toLong()))
+            service.addBan((Ban.builder()
+                    .type(BanTypes.PROFILE)
+                    .profile(pn)
+                    .source(Text.of(src.name))
+                    .reason(Text.of(message))
+                    .expirationDate(instant)
+                    .build()))
+            if (Sponge.getServer().getPlayer(pName).isPresent) {
+                Sponge.getServer().getPlayer(pName).get().kick()
+            }
+        } catch (e: ProfileNotFoundException) {
+            println("Player not found")
+        }
+        return CommandResult.success()
+    }
+
+
+}
+
+class uBanCommand : CommandExecutor {
+
+    override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+        val pName = args.getOne<String>("player").get()
+        val pn = Sponge.getServer().gameProfileManager.get(pName).get()
+        service.pardon(pn)
+        db.removeFromDb(pName)
+        return CommandResult.success()
+    }
+
+}
+
+class refreshBanCommand : CommandExecutor {
+
+    override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+        val configPath = File("banned-players.json")
+        val potentialFile = GsonConfigurationLoader.builder().setFile(configPath).build()
+        val gsonList = potentialFile.load()
+        val bList = mutableListOf<String>()
+        val mp = mutableMapOf<String, String>()
+        val banList = gsonList.getNode().getList { shit ->
+            bList.add(shit.toString())
+        }
+
+        for (i in bList) {
+            val j = i.splitToSequence(",", ignoreCase = true, limit = 6)
+            for (k in j) {
+                val f = k.replace("{", "").replace("}", "").splitToSequence("=", limit = 2).toSet().toTypedArray()
+                mp.put(f[0].trim(), f[1])
+
+            }
+            db.refrBansDb(mp)
+        }
+        return CommandResult.success()
+    }
+
+}
+
+val service = Sponge.getServiceManager().provide(BanService::class.java).get()
+val banCommand = CommandSpec.builder()
+        .arguments(
+                GenericArguments.onlyOne(GenericArguments.string(Text.of("player"))),
+                GenericArguments.doubleNum(Text.of("time(minutes)")),
+                GenericArguments.flags().permissionFlag("abplugin.aban", "s").buildWith(GenericArguments.none()),
+                GenericArguments.remainingJoinedStrings(Text.of("reason")))
+        .executor(BanCommand())
+        .build()
+
+val ubanCommand = CommandSpec.builder()
+        .arguments(GenericArguments.flags().permissionFlag("abplugin.uban", "s")
+                .buildWith(GenericArguments.onlyOne(GenericArguments.string(Text.of("player")))))
+        .executor(uBanCommand())
+        .build()
+val refrBanCommand = CommandSpec.builder()
+        .arguments(GenericArguments.flags().permissionFlag("abplugin.refrbans",
+                "s").buildWith(GenericArguments.none()))
+        .executor(refreshBanCommand())
+        .build()
